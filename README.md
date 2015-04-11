73665c31a9692eefbb04fe0c7a3056a3e001a844
Anton Koch <anton.koch@gmail.com>
2015-04-11 12:13:04 +0200

    fixed broken node client, closing #2 and #3, version bump

---
+++ README.md
@@ -0,0 +1,5 @@
+# PieceMeta API Client
+
+[![Code Climate](https://codeclimate.com/github/PieceMeta/node-piecemeta-apiclient/badges/gpa.svg)](https://codeclimate.com/github/PieceMeta/node-piecemeta-apiclient)
+
+A javascript client for the PieceMeta API
--- src/http/node-http.js
+++ src/http/node-http.js
@@ -9,7 +9,7 @@
 
         var parsedUrl = url.parse(config.url);
         var requestSetup = {
-            host: parsedUrl.host,
+            host: parsedUrl.hostname,
             port: parsedUrl.port,
             path: parsedUrl.path,
             method: config.method,
@@ -23,20 +23,25 @@
         }
 
         var req = http.request(requestSetup, function (res) {
-            var error = null;
+            var error = null,
+                result = '';
             if (res.statusCode !== 200 && res.statusCode !== 201) {
                 error = {
-                    code: statusCode,
-                    message: null
+                    code: res.statusCode,
+                    message: res.statusCode
                 };
             }
 
             res.setEncoding('utf8');
 
             res.on('data', function (data) {
-                console.log('BODY: ' + data);
-                callback(error, data);
+                result += data;
             });
+
+            res.on('end', function () {
+                callback(error, result);
+            });
+
         });
 
         req.on('error', function (err) {
